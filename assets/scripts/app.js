0f56e15e797ae75ee1879d8cbb41bc95d7637a43
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-12 22:05:38 +0300

    promisified http request with XMLHttpRequest and fetched posts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,49 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts'); // first step towards configuring a request
+    xhr.open(method, url);
 
-xhr.responseType = 'json';
+    xhr.responseType = 'json';
 
-xhr.onload = function () {
-  // console.log(xhr.response);
-  // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
-  // console.log(listOfPosts);
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    xhr.send();
+  });
+  return promise;
+}
+
+// function fetchPosts() {
+//   sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+//     responseData => {
+//       const listOfPosts = xhr.response;
+//       for (const post of listOfPosts) {
+//         const postEl = document.importNode(postTemplate.content, true);
+//         postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//         postEl.querySelector('p').textContent = post.body;
+//         listElement.append(postEl);
+//       }
+//     }
+//   );
+// }
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+fetchPosts();
