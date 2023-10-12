591d87bd9027645d744db6ce8ac8031596ff4f51
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-12 21:28:57 +0300

    utilized fetched data and output it in javascript and on the screen

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,23 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts'); // first step towards configuring a request
+
+xhr.responseType = 'json';
+
+xhr.onload = function () {
+  // console.log(xhr.response);
+  // const listOfPosts = JSON.parse(xhr.response);
+  const listOfPosts = xhr.response;
+  // console.log(listOfPosts);
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+  }
+};
 
 xhr.send();
