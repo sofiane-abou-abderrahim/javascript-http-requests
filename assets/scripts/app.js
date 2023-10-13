c5493acec1d7c36617c8cbafa6be31ec774e699d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-13 21:08:19 +0300

    handled error in xhr.onload when the server reports back a non-success status code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,7 +13,17 @@ function sendHttpRequest(method, url, data) {
     xhr.responseType = 'json';
 
     xhr.onload = function () {
-      resolve(xhr.response);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error('Something went wrong!'));
+      }
+    };
+
+    xhr.onerror = function () {
+      // console.log(xhr.response);
+      // console.log(xhr.status);
+      reject('Failed to send request!');
     };
 
     xhr.send(JSON.stringify(data));
@@ -22,17 +32,21 @@ function sendHttpRequest(method, url, data) {
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/posts'
-  );
-  const listOfPosts = responseData;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    postEl.querySelector('li').id = post.id;
-    listElement.append(postEl);
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/pos'
+    );
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.title.toUpperCase();
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
