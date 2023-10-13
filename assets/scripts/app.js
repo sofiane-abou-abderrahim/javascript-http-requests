b8e588cb484e8265641fa1d604116da54aa2858f
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-13 12:18:39 +0300

    created createPost function and sent a post request with JSON data to the API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -13,24 +13,10 @@ function sendHttpRequest(method, url) {
       resolve(xhr.response);
     };
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
   return promise;
 }
-
-// function fetchPosts() {
-//   sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
-//     responseData => {
-//       const listOfPosts = xhr.response;
-//       for (const post of listOfPosts) {
-//         const postEl = document.importNode(postTemplate.content, true);
-//         postEl.querySelector('h2').textContent = post.title.toUpperCase();
-//         postEl.querySelector('p').textContent = post.body;
-//         listElement.append(postEl);
-//       }
-//     }
-//   );
-// }
 
 async function fetchPosts() {
   const responseData = await sendHttpRequest(
@@ -46,4 +32,17 @@ async function fetchPosts() {
   }
 }
 
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId
+  };
+
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+  // and now we will actually need to enhance this method because now, we need to be able to also add a body to the request
+}
+
 fetchPosts();
+createPost('DUMMY', 'A dummy post!');
