df9cab6a4332c5ea635420131addfb3204e4722d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-13 14:53:22 +0300

    sent a delete request but not deleted the item from the ui

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -30,6 +31,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
+    postEl.querySelector('li').id = post.id;
     listElement.append(postEl);
   }
 }
@@ -53,4 +55,16 @@ form.addEventListener('submit', event => {
   const enteredContent = event.currentTarget.querySelector('#content').value;
 
   createPost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    // console.log('Clicked on button!');
+    const postId = event.target.closest('li').id;
+    // console.log(postId);
+    sendHttpRequest(
+      'DELETE',
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
 });
